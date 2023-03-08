627431567b8e3897fabf786179da22de5776d022
heric-hh <erhh.015@gmail.com>
2023-03-08 09:06:55 -0600

    Estructura inicial del proyecto

---
+++ android/app/src/main/kotlin/com/example/viewgoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.viewgoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/router/app_route.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  
+  /// * Propiedades y métodos estáticos que sirven para acceder a las rutas de la aplicacion
+  
+  static const initialRoute = 'home';   // * Ruta inicial de la aplicacion
+  
+  
+  
+}
