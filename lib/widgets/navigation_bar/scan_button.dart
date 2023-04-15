4855eb5f9aa17caf86b02b0dfbce8b0fa79caf61
heric-hh <erhh.015@gmail.com>
2023-04-14 22:03:44 -0600

    Añadido el QR scaner

--- lib/widgets/navigation_bar/scan_button.dart
+++ lib/widgets/navigation_bar/scan_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:viewgoapp/themes/app_theme.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -7,8 +8,8 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {
-        
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D88EF', 'Cancelar', false, ScanMode.QR);
       },
       backgroundColor: AppTheme.primary,
       elevation: 0,
